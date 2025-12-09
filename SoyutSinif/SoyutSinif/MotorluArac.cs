e37d7f4b28358f600d2f43c61162c42a0a35ac9c
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2025-12-09 09:45:37 +0300

    Add files via upload

---
+++ SoyutSinif/SoyutSinif/MotorluArac.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoyutSinif
+{
+    public abstract class MotorluArac
+    {
+        public int MotorHacmi { get; set; }
+        public int ModelYili { get; set; }
+        public bool OtomatikVites { get; set; }
+
+        public abstract void Calis();
+        public abstract void Dur();
+    }
+}
---
+++ SoyutSinif/SoyutSinif/Otomobil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoyutSinif
+{
+    public class Otomobil:MotorluArac
+    {
+        
+        public override void Calis()
+        {
+            MessageBox.Show("Otomobil çalıştı.");
+        }
+        public override void Dur()
+        {
+            MessageBox.Show("Otomobil durdu.");
+        }
+    }
+}
