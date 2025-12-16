e4d825fd43a7621b1afbf5cba88095f7eab4eca7
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2025-12-16 10:35:39 +0300

    Add files via upload

---
+++ staticclass2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OgrenciIslem oi = new OgrenciIslem();
+            listBox1.Items.Add("1) ==========================");
+            oi.OgrenciEkle(new Ogrenci
+            {
+                Numara = 100,
+                AdSoyad = "Nihal Öz"
+});
+            listBox1.Items.Add("Öğrenci sayısı: " +OgrenciIslem.OgrenciSayisi);
+            listBox1.Items.Add("2) ==========================");
+            oi.OgrenciEkle(new Ogrenci
+            {
+                Numara = 200,
+                AdSoyad = "İbrahim Yurt"
+});
+            listBox1.Items.Add("Öğrenci sayısı: " +OgrenciIslem.OgrenciSayisi);
+            listBox1.Items.Add("3) ==========================");
+            oi.OgrenciSil(100);
+            listBox1.Items.Add("Öğrenci sayısı: " +OgrenciIslem.OgrenciSayisi);
+        }
+    }
+}
---
+++ staticclass2/WindowsFormsApp1/OgrenciIslem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class OgrenciIslem
+    {
+        public List<Ogrenci> ogrenciler;
+        public static int OgrenciSayisi { get; set; }
+        static OgrenciIslem()
+        {
+            OgrenciSayisi = 0;
+            MessageBox.Show("Statik yapıcı çalıştı.");
+        }
+        public OgrenciIslem()
+        {
+            ogrenciler = new List<Ogrenci>();
+            MessageBox.Show("Yapıcı çalıştı.");
+        }
+        public void OgrenciEkle(Ogrenci ogr)
+        {
+            ogrenciler.Add(ogr);
+            OgrenciSayisi++;
+            MessageBox.Show("Öğrenci eklendi.");
+        }
+        public void OgrenciSil(int numara)
+        {
+            var ogr = ogrenciler.FirstOrDefault(x => x.Numara == numara);
+            if (ogr != null)
+            {
+                ogrenciler.Remove(ogr);
+                OgrenciSayisi--;
+                MessageBox.Show("Öğrenci silindi.");
+            }
+        }
+    }
+}
