23ded5f8ab50ce99f998eabff622a1ca8bfc3c9e
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2026-01-06 16:16:09 +0300

    Add files via upload

---
+++ Dizi1/Dizi1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dizi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5]; //Global Dizi
+        int index = 0; //Global Değişken
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                isimler[index] = textBox1.Text;
+                index++;
+            }
+            catch 
+            {
+                MessageBox.Show("Hatalı Giriş Yaptınız");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+               listBox1.Items.Add(isimler[i]);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sayı_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Dizi2/Dizi2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Dizi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Sayı = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 293);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 77);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Diziyi Görüntüle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(62, 218);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(386, 204);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 92);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Diziye EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Sayı
+            // 
+            this.Sayı.AutoSize = true;
+            this.Sayı.Location = new System.Drawing.Point(58, 94);
+            this.Sayı.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Sayı.Name = "Sayı";
+            this.Sayı.Size = new System.Drawing.Size(112, 20);
+            this.Sayı.TabIndex = 5;
+            this.Sayı.Text = "SAYI GİRİNİZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 470);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sayı);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Sayı;
+    }
+}
+
