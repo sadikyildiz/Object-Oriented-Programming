4088fce525afcf8c84547444356e35dd52bd2be7
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2026-02-10 09:33:38 +0300

    Add files via upload

---
+++ ArrayList2/ArrayList2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ArrayList2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList sehirler = new ArrayList();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string sehir in sehirler) //foreach (object sehir in sehirler)
+            {
+                listBox1.Items.Add(sehir);
+            }
+        }
+    }
+}
