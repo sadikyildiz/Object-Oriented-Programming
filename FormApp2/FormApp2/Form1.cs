6dd59d632dbc100007ff95e489cdb5dff5a66da7
SADIK YILDIZ <117443290+sadikyildiz@users.noreply.github.com>
2026-02-17 11:59:21 +0300

    Add files via upload

---
+++ FormApp2/FormApp2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace FormApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form1 click olayı çalışıyor.");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Form Sınıfı Uygulama 2";
+            this.BackColor = Color.Green;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(300, 150);
+
+            label1.Text = "Adınız";
+            label1.Location = new Point(10, 10);
+            label1.Size = new Size(65, 15);
+            label1.ForeColor = Color.White;
+
+            textBox1.Location = new Point(75, 10);
+            textBox1.Size = new Size(150, 15);
+            textBox1.TabIndex = 1;
+
+            button1.Text = "Tıkla";
+            button1.Location = new Point(100, 60);
+            button1.Size = new Size(100, 30);
+            button1.ForeColor = Color.White;
+            button1.BackColor = Color.Black;
+            button1.TabIndex = 2;
+
+            textBox1.KeyPress += TextBox1_KeyPress;
+            button1.Click += Button1_Click;
+
+            this.Controls.Add(label1);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(button1);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba " +textBox1.Text);
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.KeyChar = Char.ToUpper(e.KeyChar);
+        }
+
+    }
+}
---
+++ FormApp2/FormApp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FormApp2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormApp2")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba4c09f2-1eb0-4cea-8613-fc1bf6521ae9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
